48cfd5ef211def4514a0f236e0324f10228ad65b
Chadwick Butts <ccbutts@mail.usf.edu>
2025-01-30 14:26:29 -0500

    setting up backend data

---
+++ amplify/data/resource.ts
@@ -0,0 +1,23 @@
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+
+const schema = a.schema({
+    Customers: a.model({
+        customer_id: a.string(),
+        name: a.string(),
+        email: a.string(),
+        image: a.string()
+    })
+        .authorization(allow => [allow.publicApiKey()]),
+    Invoices: a.model({
+        customer_id: a.string(),
+        amount: a.integer(),
+        status: a.string(),
+        date: a.string()
+    })
+        .authorization(allow => [allow.publicApiKey()])
+});
+
+export type Schema = ClientSchema<typeof schema>;
+export const data = defineData({
+    schema
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from '../amplify/data/resource'; // Path to your backend resource definition
+
+const client = generateClient<Schema>({
+  authMode: 'apiKey'
+});
+
+const fetchCustomers = async () => {
+  const { data: customers, errors } = await client.models.Customers.list();
+}
+
+Amplify.configure(outputs);
 
 export default function RootLayout({
   children,
